ca4ef135a69d5c416221875e198d83c040ced065
krzm <kmazanek@gmail.com>
2022-01-23 21:56:49 +0100

    Update ProjectList.cs

--- Scripter.Lib/Model/ProjectList.cs
+++ Scripter.Lib/Model/ProjectList.cs
@@ -4,10 +4,21 @@ public class ProjectList : List<ProjectDTO>
 {
     public ProjectList()
     {
+        //shared core projects
         Add(new ProjectDTO("Core"));
         Add(new ProjectDTO("Console"));
-        Add(new ProjectDTO("Log.Modern.Lib"));
+
+        //log libs
         Add(new ProjectDTO("Log.Data"));
+        Add(new ProjectDTO("Log.Console.Lib"));
+        Add(new ProjectDTO("Log.Modern.Lib"));
+        //log apps
+        Add(new ProjectDTO("Log.ConsoleLib.ConsoleApp"));
         Add(new ProjectDTO("Log.Modern.ConsoleApp"));
+        //inventory libs
+        Add(new ProjectDTO("Inventory.Data"));
+        Add(new ProjectDTO("Inventory.Modern.Lib"));
+        //inventory apps
+        Add(new ProjectDTO("Inventory.Modern.ConsoleApp"));
     }
 }
