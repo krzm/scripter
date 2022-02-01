e9299954df4ceee6ac327a4d73b452f3b0c9399e
krzm <kmazanek@gmail.com>
2022-02-01 16:28:07 +0100

    projs refs changed to build, add projs to list

--- Scripter.Lib/Model/ProjectList.cs
+++ Scripter.Lib/Model/ProjectList.cs
@@ -8,5 +8,45 @@ public class ProjectList : List<ProjectDTO>
         Add(new ProjectDTO("cli-reader", "CLIReader"));
         Add(new ProjectDTO("di-helper", "CommandDotNet.IoC.Unity"));
         Add(new ProjectDTO("di-helper", "DIHelper"));
+        Add(new ProjectDTO("commanddotnet-helper", "CommandDotNet.Helper"));
+        Add(new ProjectDTO("model-helper", "ModelHelper"));
+        Add(new ProjectDTO("datatotable", "DataToTable"));
+        Add(new ProjectDTO("scripter", "Scripter"));
+
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
+        //Add(new ProjectDTO("", ""));
     }
 }
