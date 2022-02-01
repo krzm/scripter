64cf2badf778bc470ad0ee641f4973e79f5b07bd
krzm <kmazanek@gmail.com>
2022-02-01 15:34:52 +0100

    Upgrade copy script, add projects to buld list

--- Scripter.Lib.Tests/Script/CopyScriptTests.cs
+++ Scripter.Lib.Tests/Script/CopyScriptTests.cs
@@ -9,20 +9,24 @@ public class CopyScriptTests : ScriptTestBase
     private const string repoFolder = "cli-helper";
     private const string appProjFolder = "CLIHelper";
     private const string repoBuildPath = @"C:\kmazanek@gmail.com\Code\build\Build\cli-helper";  
-
+    private const string appBuildPath = $@"{repoBuildPath}\{appProjFolder}";  
+    
     [Theory]
     [InlineData(0, $"$path = \"{repoBuildPath}\"")]
     [InlineData(1, "if (-not (Test-Path $path))")]
     [InlineData(2, "{")]
     [InlineData(3, $"New-Item -Path \"{buildPath}\" -Name \"{repoFolder}\" -ItemType \"directory\"")]
     [InlineData(4, "}")]
-    [InlineData(5, $"Remove-Item \"C:\\kmazanek@gmail.com\\Code\\build\\Build\\cli-helper\\*\" -Recurse")]
-    [InlineData(6, $"$path = \"{repoBuildPath}\\{appProjFolder}\"")]
-    [InlineData(7, "if (-not (Test-Path $path))")]
-    [InlineData(8, "{")]
-    [InlineData(9, $"New-Item -Path \"{repoBuildPath}\" -Name \"{appProjFolder}\" -ItemType \"directory\"")]
-    [InlineData(10, "}")]
-    [InlineData(11, $"Copy-Item -Path \"C:\\kmazanek@gmail.com\\Code\\cli-helper\\CLIHelper\\bin\\Release\\net6.0\\publish\\*\" -Destination \"C:\\kmazanek@gmail.com\\Code\\build\\Build\\cli-helper\\CLIHelper\" -Recurse")]
+    [InlineData(5, $"$path = \"{appBuildPath}\"")]
+    [InlineData(6, "if (-not (Test-Path $path))")]
+    [InlineData(7, "{")]
+    [InlineData(8, $"New-Item -Path \"{repoBuildPath}\" -Name \"{appProjFolder}\" -ItemType \"directory\"")]
+    [InlineData(9, "}")]
+    [InlineData(10, "else")]
+    [InlineData(11, "{")]
+    [InlineData(12, $"Remove-Item \"{appBuildPath}\\*\" -Recurse")]
+    [InlineData(13, "}")]
+    [InlineData(14, $"Copy-Item -Path \"C:\\kmazanek@gmail.com\\Code\\cli-helper\\CLIHelper\\bin\\Release\\net6.0\\publish\\*\" -Destination \"C:\\kmazanek@gmail.com\\Code\\build\\Build\\cli-helper\\CLIHelper\" -Recurse")]
     public override void TestScriptContent(
         int index
         , string expected)
--- Scripter.Lib/Model/ProjectList.cs
+++ Scripter.Lib/Model/ProjectList.cs
@@ -5,5 +5,8 @@ public class ProjectList : List<ProjectDTO>
     public ProjectList()
     {
         Add(new ProjectDTO("cli-helper", "CLIHelper"));
+        Add(new ProjectDTO("cli-reader", "CLIReader"));
+        Add(new ProjectDTO("di-helper", "CommandDotNet.IoC.Unity"));
+        Add(new ProjectDTO("di-helper", "DIHelper"));
     }
 }
