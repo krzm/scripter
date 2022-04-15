a952af3125ef307fb1498a70af2494f949b6a756
krzm <kmazanek@gmail.com>
2022-04-15 23:43:52 +0200

    pull script and it's test

---
+++ Scripter.Lib/Script.Joinable/PullScript.cs
@@ -0,0 +1,45 @@
+﻿namespace Scripter;
+
+public class PullScript
+    : IScript
+{
+    private readonly IScriptParam scriptParam;
+
+    public string File
+    {
+        get
+        {
+            ArgumentNullException.ThrowIfNull(scriptParam.Project);
+            return $"{scriptParam.Project.ProjFolder}.Pull.ps1";
+        }
+    }
+
+    public PullScript(
+        IScriptParam scriptParam)
+    {
+        this.scriptParam = scriptParam;
+        ArgumentNullException.ThrowIfNull(this.scriptParam);
+    }
+
+    public string[] GetScript()
+    {
+        ArgumentNullException.ThrowIfNull(scriptParam.Project);
+        return new string[]
+        {
+            $"$repoPath = \"{scriptParam.RepoPath}\""
+            , $"$scriptPath = \"{scriptParam.ScriptPath}\""
+            , $"$proj = \"{scriptParam.Project.RepoFolder}\""
+            , "if(-not (Test-Path $repoPath))"
+            , "{"
+            , "    Write-Output \"No path: $repoPath\""
+            , "}"
+            , "else"
+            , "{"
+            , "   Set-Location $repoPath"
+            , "   $result = git pull"
+            , "   Write-Output \"$proj - $result\""
+            , "}"
+            , "Set-Location $scriptPath"
+        };
+    }
+}
