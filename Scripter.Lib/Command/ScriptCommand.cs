05aed67568bd616d89cd2c426a5592ef79bd069a
krzm <kmazanek@gmail.com>
2022-05-10 18:24:12 +0200

    add writers to cmd

--- Scripter.Lib/Command/ScriptCommand.cs
+++ Scripter.Lib/Command/ScriptCommand.cs
@@ -1,39 +1,16 @@
 using System.Windows.Input;
-using Scripter.Data.Helper;
 
 namespace Scripter.Lib;
 
 public class ScriptCommand : ICommand
 {
+    private readonly IDictionary<ScriptWriters, IScriptWriter> writerStore;
+
     public event EventHandler? CanExecuteChanged;
-    private readonly IProjectList projList;
-    private readonly List<ICodeData> codeData;
-    private readonly IScriptParam scriptParam;
-    private readonly List<IScript> projScripts;
-    private readonly List<IProjBuildAll> projBuildAllScripts;
-    private readonly List<IBuildAll> buildAllScripts;
 
-    public ScriptCommand(
-        IProjectList projList
-        , List<ICodeData> codeData
-        , IScriptParam scriptParam
-        , List<IScript> projectScripts
-        , List<IProjBuildAll> projBuildAllScripts
-        , List<IBuildAll> buildAllScripts)
+    public ScriptCommand(IDictionary<ScriptWriters, IScriptWriter> writerStore)
     {
-        this.projList = projList;
-        this.codeData = codeData;
-        this.scriptParam = scriptParam;
-        this.projScripts = projectScripts;
-        this.projBuildAllScripts = projBuildAllScripts;
-        this.buildAllScripts = buildAllScripts;
-
-        ArgumentNullException.ThrowIfNull(this.projList);
-        ArgumentNullException.ThrowIfNull(this.codeData);
-        ArgumentNullException.ThrowIfNull(this.scriptParam);
-        ArgumentNullException.ThrowIfNull(this.projScripts);
-        ArgumentNullException.ThrowIfNull(this.projBuildAllScripts);
-        ArgumentNullException.ThrowIfNull(this.buildAllScripts);
+        this.writerStore = writerStore;
     }
 
     public void Invalidate()
@@ -45,74 +22,8 @@ public class ScriptCommand : ICommand
 
     public void Execute(object? parameter)
     {
-        WriteProjectScripts();
-        WriteProjBuildAllScripts();
-        WriteBuildAllScripts();
-    }
-
-    private void WriteProjectScripts()
-    {
-        foreach (var project in projList.Projects)
-        {
-            SetProject(project);
-            ArgumentNullException.ThrowIfNull(scriptParam.Project);
-            foreach (var script in projScripts)
-            {
-                if (scriptParam.Project.IsApp == false
-                    && script is CopyAppScript) continue;
-                WriteScript(script);
-            }
-        }
-    }
-
-    private void SetProject(ProjectDTO project)
-    {
-        scriptParam.Project = new ProjectDTO(
-            project.RepoFolder
-            , project.ProjFolder
-            , IsApp: project.IsApp);
-    }
-
-    private void WriteScript(IScript script)
-    {
-        ArgumentNullException.ThrowIfNull(scriptParam.Project);
-        File.WriteAllLines(
-            Path.Combine(scriptParam.ScriptPath
-                , script.File)
-            , script.GetScript());
-    }
-
-    private void WriteProjBuildAllScripts()
-    {
-        foreach (var script in projBuildAllScripts)
-        {
-            WriteScript(script);   
-        }
-    }
-
-    private void WriteScript(IProjBuildAll buildScript)
-    {
-        ArgumentNullException.ThrowIfNull(scriptParam.Project);
-        File.WriteAllLines(
-            Path.Combine(scriptParam.ScriptPath
-                , buildScript.File)
-                , buildScript.GetScript());
-    }
-
-    private void WriteBuildAllScripts()
-    {
-        foreach (var script in buildAllScripts)
-        {
-            WriteScript(script);   
-        }
-    }
-
-    private void WriteScript(IBuildAll buildScript)
-    {
-        ArgumentNullException.ThrowIfNull(scriptParam.Project);
-        File.WriteAllLines(
-            Path.Combine(scriptParam.ScriptPath
-                , buildScript.File)
-            , buildScript.GetScript());
+        writerStore[ScriptWriters.Project].WriteScripts();
+        writerStore[ScriptWriters.ProjectBuildAll].WriteScripts();
+        writerStore[ScriptWriters.BuildAll].WriteScripts();
     }
 }
