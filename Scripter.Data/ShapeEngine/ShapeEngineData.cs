f28175c29459a6b15dfdf503e77cc98e7bac22f7
krzm <kmazanek@gmail.com>
2022-05-12 23:40:17 +0200

    Upd shape engine data

--- Scripter.Data/ShapeEngine/ShapeEngineData.cs
+++ Scripter.Data/ShapeEngine/ShapeEngineData.cs
@@ -5,21 +5,13 @@ namespace Scripter.Data;
 public class ShapeEngineData 
     : IndependantLibData
 {
-    private ProjectDTO? shapeModel;
     private ProjectDTO? simCore;
+    private ProjectDTO? shapeModel;
 
     protected override void SetAllData()
     {
         base.SetAllData();
         ArgumentNullException.ThrowIfNull(Vector2);
-        shapeModel = Set(
-            repo: "shape-model"
-            , project:  "Shape.Model"
-            , isApp: false
-            , isWpf: true
-            , lastCheck: new DateOnly(2022, 5, 9)
-            , libs: Vector2
-            );
         simCore = Set(
             repo: "sim-core"
             , project:  "Sim.Core"
@@ -27,6 +19,15 @@ public class ShapeEngineData
             , isWpf: true
             , lastCheck: new DateOnly(2022, 5, 12)
             , libs: Vector2
+            );
+        shapeModel = Set(
+            repo: "shape-model"
+            , project:  "Shape.Model"
+            , isApp: false
+            , isWpf: true
+            , lastCheck: new DateOnly(2022, 5, 9)
+            , Vector2
+            , simCore
             );
     }
 }
